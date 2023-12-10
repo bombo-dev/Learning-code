854b2154e6e86354a4902f8542d6388dd8e001a0
Bombo <bombo96@chungwoon.ac.kr>
2023-12-10 22:32:32 +0900

    RedisTemplate Serialize Error 해결

--- redis/src/main/java/com/example/redis/global/config/RedisConfiguration.java
+++ redis/src/main/java/com/example/redis/global/config/RedisConfiguration.java
@@ -1,12 +1,24 @@
 package com.example.redis.global.config;
 
 import com.example.redis.global.property.RedisProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @EnableRedisRepositories(basePackages = "com.example.redis.domain.redis")
@@ -21,5 +33,34 @@ public class RedisConfiguration {
         int port = redisProperties.getPort();
 
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean(name = "redisObjectMapper")
+    public ObjectMapper redisObjectMapper() {
+        BasicPolymorphicTypeValidator validator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(validator, ObjectMapper.DefaultTyping.NON_FINAL);
+        return objectMapper;
+    }
+
+    @Bean(name = "defaultRedisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory,
+                                                           @Qualifier("redisObjectMapper") ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean(name = "stringRedisTemplate")
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        return stringRedisTemplate;
     }
 }
--- redis/src/test/java/com/example/redis/redistemplate/RedisTemplateTest.java
+++ redis/src/test/java/com/example/redis/redistemplate/RedisTemplateTest.java
@@ -1,4 +1,77 @@
 package com.example.redis.redistemplate;
 
+import com.example.redis.domain.redis.Fruits;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class RedisTemplateTest {
+
+    @Resource(name = "defaultRedisTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource(name = "stringRedisTemplate")
+    private StringRedisTemplate stringRedisTemplate;
+
+    @DisplayName("Resource를 사용하여 빈의 이름에 맞는 빈을 주입받는다.")
+    @Test
+    void resourceDependencyInjection() {
+        // when & then
+        assertThat(redisTemplate.getClass().getSimpleName()).isEqualTo("RedisTemplate");
+        assertThat(stringRedisTemplate.getClass().getSimpleName()).isEqualTo("StringRedisTemplate");
+    }
+
+    @DisplayName("redisTemplate key-value 형식으로 기본형 타입 저장")
+    @Test
+    void saveValueOpsPrimitiveType() {
+        // given
+        ValueOperations<String, Object> stringObjectValueOperations = redisTemplate.opsForValue();
+
+        String key = "KEY";
+        stringObjectValueOperations.set(key, 1);
+        stringObjectValueOperations.set(key, 2);
+        stringObjectValueOperations.set(key, 3);
+
+        // when
+        Integer value = (Integer) stringObjectValueOperations.get(key);
+        Set<String> keys = redisTemplate.keys(key);
+
+        // then
+        assertThat(keys.size()).isEqualTo(1);
+        assertThat(value).isEqualTo(3);
+    }
+
+    @DisplayName("redisTemplate key-value 형식으로 객체 타입 저장")
+    @Test
+    void saveValueOpsReferenceType() {
+        // given
+        ValueOperations<String, Object> stringObjectValueOperations = redisTemplate.opsForValue();
+        String key = "ReferenceKey";
+
+        LocalDateTime serverTime = LocalDateTime.now();
+
+        Fruits apple = Fruits.createFruit("사과", 10, serverTime);
+        Fruits banana = Fruits.createFruit("바나나", 15, serverTime);
+
+        stringObjectValueOperations.set(key, apple);
+        stringObjectValueOperations.set(key, banana);
+        Set<String> keys = redisTemplate.keys(key);
+
+        // when
+        Fruits value = (Fruits) stringObjectValueOperations.get(key);
+
+        // then
+        assertThat(keys.size()).isEqualTo(1);
+        assertThat(value).usingRecursiveComparison().isEqualTo(banana);
+    }
 }
