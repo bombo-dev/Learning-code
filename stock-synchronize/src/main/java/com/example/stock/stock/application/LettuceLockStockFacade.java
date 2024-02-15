d013a5a4951f066a691b51778bce4c7a8862be01
Bombo <bombo96@chungwoon.ac.kr>
2024-02-15 21:52:13 +0900

    재고 시스템 동시성 이슈 해결하기 - Redis Lettuce(Spin lock) -

---
+++ stock-synchronize/src/main/java/com/example/stock/stock/application/LettuceLockStockFacade.java
@@ -0,0 +1,28 @@
+package com.example.stock.stock.application;
+
+import com.example.stock.stock.repository.RedisLockRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decreaseStock(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
