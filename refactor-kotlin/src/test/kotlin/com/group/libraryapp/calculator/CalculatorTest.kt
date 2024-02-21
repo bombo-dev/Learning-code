833ba4c7ff3f891728a50c31b7a5b6ae8a7235d4
Bombo <bombo96@chungwoon.ac.kr>
2024-02-21 09:27:09 +0900

    kotlin 테스트 코드 예외 다른 형태

--- refactor-kotlin/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ refactor-kotlin/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -61,9 +61,11 @@ class CalculatorTest {
         val calculator = Calculator(5)
 
         // when & then
-        assertThrows<IllegalArgumentException> {
+        val message = assertThrows<IllegalArgumentException> {
             calculator.divide(0)
-        }
+        }.message
+
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다.");
     }
 
     @Test
