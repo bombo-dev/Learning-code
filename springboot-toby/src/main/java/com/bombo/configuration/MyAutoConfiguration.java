cb550eac0314c14d80e974e02f3e4af3f53121de
TI-Bombo <jwmoon@trenbe.com>
2024-03-25 07:56:56 +0900

    change dynamic bean info using ImportCandidates

--- springboot-toby/src/main/java/com/bombo/configuration/MyAutoConfiguration.java
+++ springboot-toby/src/main/java/com/bombo/configuration/MyAutoConfiguration.java
@@ -1,14 +1,13 @@
 package com.bombo.configuration;
 
-import com.bombo.configuration.autoconfiguration.EnableSelectImporterAutoConfiguration;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Configuration;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(EnableSelectImporterAutoConfiguration.class)
-public @interface EnableMyAutoConfiguration {
+@Configuration
+public @interface MyAutoConfiguration {
 }
--- springboot-toby/src/main/java/com/bombo/configuration/autoconfiguration/EnableSelectImporterAutoConfiguration.java
+++ springboot-toby/src/main/java/com/bombo/configuration/autoconfiguration/EnableSelectImporterAutoConfiguration.java
@@ -1,15 +1,22 @@
 package com.bombo.configuration.autoconfiguration;
 
+import com.bombo.configuration.MyAutoConfiguration;
+import java.util.stream.StreamSupport;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class EnableSelectImporterAutoConfiguration implements DeferredImportSelector {
 
+    private final ClassLoader classLoader;
+
+    public EnableSelectImporterAutoConfiguration(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.bombo.configuration.autoconfiguration.DispatcherServletConfiguration",
-                "com.bombo.configuration.autoconfiguration.TomcatWebServerConfiguration",
-        };
+        ImportCandidates candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
