f8a8505cdb937c2ff45bdd0e3dc9d2af4df23ef4
Bombo <bombo96@chungwoon.ac.kr>
2024-03-27 08:19:00 +0900

    apply conditional in configuration

--- springboot-toby/src/main/java/com/bombo/configuration/autoconfiguration/TomcatWebServerConfiguration.java
+++ springboot-toby/src/main/java/com/bombo/configuration/autoconfiguration/TomcatWebServerConfiguration.java
@@ -4,8 +4,13 @@ import com.bombo.configuration.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfiguration.TomcatCondition.class)
 public class TomcatWebServerConfiguration {
 
     @Bean("tomcatWebServerFactory")
@@ -13,4 +18,11 @@ public class TomcatWebServerConfiguration {
         return new TomcatServletWebServerFactory();
     }
 
+    static class TomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
 }
