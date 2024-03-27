145de9a6a1c685c3433029c77913eacf52137bcc
Bombo <bombo96@chungwoon.ac.kr>
2024-03-28 08:25:20 +0900

    ApplicationContext auto Test using meta annotation

--- springboot-toby/src/test/java/com/bombo/springboottoby/learn/ConditionalTest.java
+++ springboot-toby/src/test/java/com/bombo/springboottoby/learn/ConditionalTest.java
@@ -1,5 +1,9 @@
 package com.bombo.springboottoby.learn;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -18,22 +22,34 @@ public class ConditionalTest {
     @Test
     void conditional() {
         // true
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
-        ac.register(Config1.class);
-        ac.refresh();
-
-        MyBean bean = ac.getBean(MyBean.class);
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context ->  {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
 
         // false
-        AnnotationConfigApplicationContext ac2 = new AnnotationConfigApplicationContext();
-        ac2.register(Config2.class);
-        ac2.refresh();
-
-        MyBean bean2 = ac2.getBean(MyBean.class);
+        ApplicationContextRunner contextRunner2 = new ApplicationContextRunner();
+        contextRunner2.withUserConfiguration(Config2.class)
+                .run(context ->  {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config2.class);
+                });
     }
 
-    @Configuration
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
     @Conditional(TrueCondition.class)
+    @interface TrueConditional {}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(FalseCondition.class)
+    @interface FalseConditional {}
+
+    @Configuration
+    @TrueConditional
     static class Config1 {
 
         @Bean
@@ -43,7 +59,7 @@ public class ConditionalTest {
     }
 
     @Configuration
-    @Conditional(FalseCondition.class)
+    @FalseConditional
     static class Config2 {
 
         @Bean
