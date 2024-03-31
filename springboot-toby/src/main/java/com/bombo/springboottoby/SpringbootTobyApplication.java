04dbf468788810172ab7ebd39a3084cf44b3845d
Bombo <bombo96@chungwoon.ac.kr>
2024-03-31 14:37:37 +0900

    enable Environment CLI using ApplicationRunner

--- springboot-toby/src/main/java/com/bombo/springboottoby/SpringbootTobyApplication.java
+++ springboot-toby/src/main/java/com/bombo/springboottoby/SpringbootTobyApplication.java
@@ -1,10 +1,22 @@
 package com.bombo.springboottoby;
 
 import com.bombo.configuration.MySpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class SpringbootTobyApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTobyApplication.class, args);
