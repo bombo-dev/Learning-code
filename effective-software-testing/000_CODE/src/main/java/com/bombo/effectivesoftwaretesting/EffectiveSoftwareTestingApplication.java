d5bc7764d40b0a750a1bb5c105be913913eeb938
TI-Bombo <jwmoon@trenbe.com>
2024-09-01 13:46:16 +0900

    Chapter 01 추가

---
+++ effective-software-testing/000_CODE/src/main/java/com/bombo/effectivesoftwaretesting/EffectiveSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.bombo.effectivesoftwaretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveSoftwareTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveSoftwareTestingApplication.class, args);
+    }
+
+}
---
+++ effective-software-testing/000_CODE/src/test/java/com/bombo/effectivesoftwaretesting/EffectiveSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombo.effectivesoftwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveSoftwareTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
