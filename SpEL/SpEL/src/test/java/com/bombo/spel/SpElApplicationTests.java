8e5dfeaec2e8b96dc697be0d69def2aa61f7d51f
TI-Bombo <jwmoon@trenbe.com>
2024-10-13 17:12:42 +0900

    SpEL 생성

---
+++ SpEL/SpEL/src/test/java/com/bombo/spel/SpElApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombo.spel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpElApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
