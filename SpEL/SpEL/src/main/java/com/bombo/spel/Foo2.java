9a2318b398b109ea5336ed70ac44f7d0db3f4d23
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-13 18:11:01 +0900

    SpEL 기본 테스트 이너 객체 추가

---
+++ SpEL/SpEL/src/main/java/com/bombo/spel/Foo2.java
@@ -0,0 +1,14 @@
+package com.bombo.spel;
+
+public class Foo2 {
+
+    private Foo foo;
+
+    public Foo2(Foo foo) {
+        this.foo = foo;
+    }
+
+    public Foo getFoo() {
+        return foo;
+    }
+}
--- SpEL/SpEL/src/test/java/com/bombo/spel/SpELTest.java
+++ SpEL/SpEL/src/test/java/com/bombo/spel/SpELTest.java
@@ -45,6 +45,16 @@ class SpELTest {
         Assertions.assertThat(result).isEqualTo(expectedValue);
     }
 
+    @DisplayName("Dot Notation으로 객체 내의 객체에도 접근 할 수 있다.")
+    @Test
+    void access_inner_object() {
+        String expectedValue = "Hello";
+
+        SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+        String result = (String) spelExpressionParser.parseExpression("new com.bombo.spel.Foo2(new com.bombo.spel.Foo('Hello')).foo.dummy").getValue();
+        Assertions.assertThat(result).isEqualTo(expectedValue);
+    }
+
     @DisplayName("Expression 객체의 getValue에 Class Type을 명시하면 캐스팅을 제거 할 수 있다.")
     @Test
     void expression_getValue_include_classType() {
