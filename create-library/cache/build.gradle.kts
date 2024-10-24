61f9abc5629ec3c3f5d78afc79767631351485a8
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-24 23:32:11 +0900

    redisson library 추가

--- create-library/cache/build.gradle.kts
+++ create-library/cache/build.gradle.kts
@@ -9,8 +9,11 @@ repositories {
     mavenCentral()
 }
 
+val SPRING_BOOT_VERISON = "2.7.5"
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-aop:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-aop:$SPRING_BOOT_VERISON")
+    implementation("org.redisson:redisson-spring-boot-starter:$SPRING_BOOT_VERISON")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
