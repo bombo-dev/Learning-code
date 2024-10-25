d97b9ba071648ddec362af8560e7e9479e515364
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-25 09:04:48 +0900

    분산 락 AOP 구현

--- create-library/cache/build.gradle.kts
+++ create-library/cache/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.bombo"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -13,8 +13,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop:2.7.5")
     implementation("org.redisson:redisson-spring-boot-starter:3.36.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(kotlin("stdlib-jdk8"))
 }
 
