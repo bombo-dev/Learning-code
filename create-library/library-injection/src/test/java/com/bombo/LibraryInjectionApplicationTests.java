b2c03784e96342a4f78e202bce4eea0f8ffcbb36
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-25 09:19:37 +0900

    library 테스트 프로젝트 생성

---
+++ create-library/library-injection/src/test/java/com/bombo/LibraryInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
