15c3462545b8ccece90cd868cfe19ec8048af91a
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-10-27 11:58:55 +0900

    jdk 11 버전 다운 그레이드

--- create-library/cache/build.gradle.kts
+++ create-library/cache/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop:2.7.5")
     implementation("org.redisson:redisson-spring-boot-starter:3.17.7")
-    implementation(kotlin("stdlib-jdk11"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
