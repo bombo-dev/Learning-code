b990fde78f01e84e66f2f00370f887ae95153925
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-11-03 23:52:16 +0900

    분산 환경 서버 추가

---
+++ webflux/테스트 프로젝트/webflux/microservice-webflux/libs/http-core/src/test/java/com/bombo/httpcore/HttpCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombo.httpcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
