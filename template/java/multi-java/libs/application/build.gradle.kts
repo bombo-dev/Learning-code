a73405de602b1b8c63c87071d1ee16accab0ba2f
Bombo-Dev <bombo96@chungwoon.ac.kr>
2024-11-12 08:11:52 +0900

    http 단 방향 의존성 추가

--- template/java/multi-java/libs/application/build.gradle.kts
+++ template/java/multi-java/libs/application/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation(project(":libs:client"))
     runtimeOnly(project(":libs:storage:db-core"))
 }
--- template/java/multi-java/settings.gradle.kts
+++ template/java/multi-java/settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "multi-java"
 include("apps:internal-api")
 include("libs:application")
 include("libs:storage:db-core")
+include("libs:client")
